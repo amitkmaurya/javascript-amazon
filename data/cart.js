2c6203eb735978818bfa9702f70df54fa1d7d737
Amit Kumar <amitkuswaha332@gmail.com>
2025-10-14 15:58:24 +0530

    one item added to cart

--- data/cart.js
+++ data/cart.js
@@ -10,6 +10,11 @@ if (!cart) {
   productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
   quantity: 1,
   deliveryOptionId: '2'
+},
+{
+  productId: "183d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+  quantity: 5,
+  deliveryOptionId: '3'
 }];
 }
 
